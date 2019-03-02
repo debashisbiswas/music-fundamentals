b3de414a95dcc26a2bec0a87e26371aec473201b
Debashis Biswas <debashis.biswas@asu.edu>
2019-03-02 13:32:46 -0700

    added scales and exercises files, and added code to parse them into lists

--- music-fundamentals.py
+++ music-fundamentals.py
@@ -1,1 +1,18 @@
-print('Music Fundamentals')
+print('--- MUSIC FUNDAMENTALS ---\n')
+
+# open scales file and populate list
+scales_file = open('data/scales.txt', 'r')
+scales_list = list()
+for line in scales_file.read().splitlines():
+    scales_list.append(line)
+
+scales_file.close()
+
+# open exercises file and populate list
+exercises_file = open('data/exercises.txt', 'r')
+exercises_list = list()
+
+for line in exercises_file.read().splitlines():
+    exercises_list.append(line)
+
+exercises_file.close()
